7bb71e0b95c64443bd35bac2dfe26495186291c5
hendisantika <hendisantika@yahoo.co.id>
2024-03-30 09:10:03 +0700

    Add Redirect Controller🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootopensearch/controller/Redirect.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springbootopensearch.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-opensearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/30/24
+ * Time: 09:09
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class Redirect {
+}
