ec49cfc19166f2ee62ed47306d2971e2946e3cc1
hendisantika <hendisantika@yahoo.co.id>
2024-03-30 09:01:37 +0700

    Add RestClientConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootopensearch/config/RestClientConfig.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.springbootopensearch.config;
+
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.data.client.orhlc.AbstractOpenSearchConfiguration;
+import org.opensearch.data.client.orhlc.ClientConfiguration;
+import org.opensearch.data.client.orhlc.RestClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-opensearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/30/24
+ * Time: 09:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RestClientConfig extends AbstractOpenSearchConfiguration {
+
+    @Override
+    @Bean
+    public RestHighLevelClient opensearchClient() {
+
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
