d4e2faf56ba23a7a43b2dcc5dfd3d5792b2d2dc2
hendisantika <hendisantika@yahoo.co.id>
2024-03-30 09:14:02 +0700

    Add SingletonContainer🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/springbootopensearch/SingletonContainer.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.springbootopensearch;
+
+import org.testcontainers.containers.GenericContainer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-opensearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/30/24
+ * Time: 09:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class SingletonContainer {
+    private static final GenericContainer<?> openSearchContainer = createOpenSearchContainer();
+    private static final boolean isStarted = false;
+}
