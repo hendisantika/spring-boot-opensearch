81728f5a4004d7f611a49bf1cd23fc37ac9fd3d8
hendisantika <hendisantika@yahoo.co.id>
2024-03-30 09:11:50 +0700

    Add createBook in BookController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootopensearch/controller/BookController.java
+++ src/main/java/id/my/hendisantika/springbootopensearch/controller/BookController.java
@@ -1,10 +1,15 @@
 package id.my.hendisantika.springbootopensearch.controller;
 
+import id.my.hendisantika.springbootopensearch.dto.BookDto;
+import id.my.hendisantika.springbootopensearch.exception.DuplicateIsbnException;
 import id.my.hendisantika.springbootopensearch.model.Book;
 import id.my.hendisantika.springbootopensearch.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +37,11 @@ public class BookController {
     @GetMapping
     public List<Book> getAllBooks() {
         return bookService.getAll();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping
+    public Book createBook(@Valid @RequestBody BookDto book) throws DuplicateIsbnException {
+        return bookService.create(BookDto.transform(book));
     }
 }
---
+++ src/main/java/id/my/hendisantika/springbootopensearch/dto/BookDto.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.springbootopensearch.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-opensearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/30/24
+ * Time: 09:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookDto {
+}
