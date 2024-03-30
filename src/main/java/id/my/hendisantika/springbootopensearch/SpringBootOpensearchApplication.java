72908bbe2ed2101c892a458e96fcd74859af87ac
hendisantika <hendisantika@yahoo.co.id>
2024-03-30 08:55:37 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootopensearch/SpringBootOpensearchApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootopensearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpensearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpensearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootopensearch/SpringBootOpensearchApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootopensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpensearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
