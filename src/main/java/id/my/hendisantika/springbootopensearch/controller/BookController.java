4960f35a8ce8982a9e5260deca9ddf7773ff9a8c
hendisantika <hendisantika@yahoo.co.id>
2024-03-30 09:13:15 +0700

    Add updateBook in BookController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootopensearch/controller/BookController.java
+++ src/main/java/id/my/hendisantika/springbootopensearch/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,5 +59,11 @@ public class BookController {
     @GetMapping(value = "/query")
     public List<Book> getBooksByAuthorAndTitle(@RequestParam(value = "title") String title, @RequestParam(value = "author") String author) {
         return bookService.findByTitleAndAuthor(title, author);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = "/{id}")
+    public Book updateBook(@PathVariable String id, @RequestBody BookDto book) throws BookNotFoundException {
+        return bookService.update(id, BookDto.transform(book));
     }
 }
